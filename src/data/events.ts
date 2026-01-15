a25026556ab211b0e141a6b89f24c0913365e833
skytomo <skytomo221@gmail.com>
2026-01-15 23:50:11 +0900

    feat!: remove description and content fields from events table

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,10 +2,8 @@ export type Event = {
 	slug: string;
 	title: string;
 	date: string;
-	description?: string;
 	tags?: string[];
 	tweet?: string;
-	content?: string[];
 };
 
 export const events: Event[] = [
@@ -13,18 +11,14 @@ export const events: Event[] = [
 		slug: '2026-01-18',
 		title: '第9回シピルカくん集会',
 		date: '2026-01-18T21:30:00+09:00',
-		description: '',
 		tweet: '2007825201376182374',
 		tags: ['VRChat', 'シピルカくん集会'],
-		content: ['明けましておめでとうございます。今年もシピルカくん集会をよろしくお願いします☆', '1月18日（日） に「第9回シピルカくん集会」を開催いたします。集会の詳細および行動規範については、以下のツリーをご確認ください。皆様のご参加をお待ちしております！'],
 	},
 	{
 		slug: '2025-07-27',
 		title: '第8回シピルカくん集会',
 		date: '2025-07-27T21:30:00+09:00',
-		description: 'Xで告知した過去回のアーカイブです。',
 		tweet: '1947815239791325220',
 		tags: ['VRChat', 'シピルカくん集会'],
-		content: ['当日はスタッフの案内に従ってください。'],
 	},
 ];
