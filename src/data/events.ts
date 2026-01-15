47ac97e51c9aa864737b3ae67558bd19a94fb314
skytomo <skytomo221@gmail.com>
2026-01-15 23:48:00 +0900

    feat: add tweet embed input component

--- src/data/events.ts
+++ src/data/events.ts
@@ -4,7 +4,7 @@ export type Event = {
 	date: string;
 	description?: string;
 	tags?: string[];
-	xPost?: string;
+	tweet?: string;
 	content?: string[];
 };
 
@@ -14,6 +14,7 @@ export const events: Event[] = [
 		title: '第9回シピルカくん集会',
 		date: '2026-01-18T21:30:00+09:00',
 		description: '',
+		tweet: '2007825201376182374',
 		tags: ['VRChat', 'シピルカくん集会'],
 		content: ['明けましておめでとうございます。今年もシピルカくん集会をよろしくお願いします☆', '1月18日（日） に「第9回シピルカくん集会」を開催いたします。集会の詳細および行動規範については、以下のツリーをご確認ください。皆様のご参加をお待ちしております！'],
 	},
@@ -22,7 +23,7 @@ export const events: Event[] = [
 		title: '第8回シピルカくん集会',
 		date: '2025-07-27T21:30:00+09:00',
 		description: 'Xで告知した過去回のアーカイブです。',
-		xPost: 'https://x.com/Shipilka_club/status/1947815239791325220',
+		tweet: '1947815239791325220',
 		tags: ['VRChat', 'シピルカくん集会'],
 		content: ['当日はスタッフの案内に従ってください。'],
 	},
